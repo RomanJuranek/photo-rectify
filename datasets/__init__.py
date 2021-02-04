1bd786fb49beae612240871f0e3fa045824fb0af
Roman Juranek <ijuranek@fit.vutbr.cz>
2021-02-04 20:49:32 +0100

    Dataset wrapper for mask caching

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -29,9 +29,14 @@ There might be other keys like "filename", "pp", etc. that can be emplyed
 for horizon detecion or evaluation purposes.
 """
 
+from itertools import count
 from pathlib import Path
+import random
+from typing import Any, Sequence
 
+import numpy as np
 from skimage.io import imread
+from skimage.transform import resize
 
 from .ecd import EurasianCities
 from .gp3k import GeoPose3KDataset
@@ -53,3 +58,34 @@ class ImageDirDataset:
 
     def __getitem__(self, idx):
         return dict(image=imread(self.fs[idx]))
+
+
+
+class DataWithCachedMasks:
+    def __init__(self, dataset, redis, cache_target, key_prefix):
+        self.dataset = dataset
+        self.redis = redis
+        self.cache_target = cache_target
+        self.key_prefix = key_prefix
+
+    def __getitem__(self, idx):
+        image_dict:dict = self.dataset[idx]
+        key = f"{self.key_prefix}:{idx}"
+        identifier = self.redis.get(key).decode()
+        #print(self.cache_target, identifier)
+        data = np.load(self.cache_target / identifier)
+        arr = [x[...,None] for x in [data["arr_0"], data["arr_1"], data["arr_2"]]]
+        masks = np.concatenate(arr, axis=-1)
+        masks = resize(masks, image_dict["image"].shape[:2] + (masks.shape[-1],), preserve_range=True).astype("f")
+        image_dict.update(masks=masks)
+        return image_dict
+
+    def __len__(self):
+        return len(self.dataset)
+
+
+def random_iterator(seq:Sequence[Any], max_len=None) -> Any:
+    for k in count():
+        if k > max_len:
+            return
+        yield random.choice(seq)
